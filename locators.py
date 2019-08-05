c0ff105c06d33a8ddaadc1959dda66fc751ecad6
Marat Gainutdinov <Marat.Gainutdinov@icl-services.com>
2019-08-05 18:56:30 +0300

    completed the practice exercises

--- locators.py
+++ locators.py
@@ -3,3 +3,9 @@ import time
 
 class WikipediaHomepage(object):
 	Random_Link = (By.CSS_SELECTOR, '#n-randompage')
+
+class WikipediaArticle(object):
+	First_Heading = (By.CSS_SELECTOR, '.firstHeading')
+	Page_Info = (By.LINK_TEXT, 'Page information')
+	Search_Box = (By.NAME, 'search')
+	Logo = (By.XPATH, '/html/body/div[5]/div[2]/div[1]/a')
